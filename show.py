10622c25649290aee624359ccd1f53386d304c53
CHJ <gkswns3708@naver.com>
2024-11-27 08:47:10 +0300

    2024/11/27

--- show.py
+++ show.py
@@ -17,11 +17,13 @@ from omegaconf import DictConfig
 import sys
 
 import torch
+import torch.nn as nn
 from brats import get_datasets
-from utils.visualizer import visualize_abnormal_area, get_labelled_image, visualize_data_gif, get_prediction_label_image
-from utils.general import visualize_data_sample
+# from utils.visualizer import visualize_abnormal_area
+from utils.visualizer import get_labelled_image, visualize_data_gif, get_show_image
+# from utils.general import visualize_data_sample
 
-# from networks.models.nnformer.nnformer 
+from networks.models.nnformer.nnFormer_tumor import nnFormer
 
 # Logger
 logger = logging.getLogger(__name__)
@@ -36,6 +38,7 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @hydra.main(config_name='configs', config_path='conf', version_base=None)
 def show_result(cfg: DictConfig):
@@ -48,24 +51,31 @@ def show_result(cfg: DictConfig):
     # Hydra에서 type 값을 가져오기
     # TODO: Show 를 위한 Config 설정
     # ./conf/configs.yaml의 'type' key를 확인해 값이 있으면 가져오고 없으면 "get-gif"로 사용
-    visualization_type = cfg.get("type", "label_get-gif") 
+    visualization_type = cfg.show.type
+    patient_id = cfg.show.patient_id
+    print(visualization_type, "- visualization_type")
 
     # Load data
-    dataset = get_datasets(cfg.dataset.dataset_folder, "test")
+    dataset = get_datasets(cfg.show.dataset_folder, "test")
     data_loader = torch.utils.data.DataLoader(dataset,
                                               batch_size=1,
                                               shuffle=False, num_workers=8,
                                               pin_memory=True)
 
     # Batch of data
-    batch = next(iter(data_loader))
-    image, label = batch["image"], batch['label'][:, 0]
+    if 0 <= patient_id < len(data_loader):
+        batch = data_loader.dataset[patient_id]
+        image, label = batch["image"].unsqueeze(0), batch['label'].unsqueeze(0)
+
+    # batch = next(iter(data_loader))
+    # image, label = batch["image"], batch['label']
+    print(image.shape, label.shape, "- get_labelled_image 입력전 image, label shape")
     logger.info('visualizing an image with label')
 
     # Visualize
-    if visualization_type == "show-abnormal-image":
-        visualize_abnormal_area(image, label)
-    elif visualization_type == "label_get-gif":
+    # if visualization_type == "show-abnormal-image":
+    #     visualize_abnormal_area(image, label)
+    if visualization_type == "label_get-gif":
         print(image.shape, label.shape, "- get_labelled_image 입력전 image, label shape")
         labelled_img = get_labelled_image(image, label)
         visualize_data_gif(labelled_img)
@@ -73,20 +83,35 @@ def show_result(cfg: DictConfig):
         # TODO: prediction image와 label을 한번에 비교할 수 있도록 하는 gif 생성.
         # print(image.shape, label.shape, "- get_labelled_image 입력전 image, label shape")
         # TODO: Model 생성 부분 config로 수정할 수 있도록 함.
+        # nnFormer = nnFormer_tumor.py
         model = nnFormer(crop_size=np.array([128, 128, 128]), 
-                         embedding_dim=96, 
-                         input_channels=in_channels, 
-                         num_classes=num_classes, 
-                         depths=[2, 2, 2, 2], 
-                         num_heads=[3, 6, 12, 24], 
-                         deep_supervision=False,
-                         conv_op=nn.Conv3d,
-                         patch_size= [4,4,4], 
-                         window_size=[4,4,8,4]).to(device)
-        labelled_img = get_prediction_label_image(image, label, prediction_image)
-        visualize_data_gif(labelled_img)
-    elif visualization_type == "show-case":
-        visualize_data_sample(cfg.paths.test_patient)
+                        embedding_dim=96, 
+                        input_channels=4, 
+                        num_classes=3, 
+                        depths=[2, 2, 2, 2], 
+                        num_heads=[3, 6, 12, 24], 
+                        deep_supervision=False,
+                        conv_op=nn.Conv3d,
+                        patch_size=[4,4,4], 
+                        window_size=[4,4,8,4]).to(device)
+        checkpoint = torch.load(cfg.show.model_path)
+        model.load_state_dict(checkpoint['model'])
+        model.eval()  # 모델을 평가 모드로 전환
+        model.to(device)
+
+        # Inference with no_grad
+        with torch.no_grad():
+            image = image.to(device)
+            prediction_image = model(image).int()  # 예측 수행
+            prediction_image = (prediction_image > 0.5)  # Thresholding to binary
+            image = image.to('cpu')
+            label = label.to('cpu')
+            prediction_image = prediction_image.to('cpu')
+
+        # 시각화를 위한 데이터 준비
+        labelled_imgs, predicted_imgs, overlay_predicted_imgs = get_show_image(image, label, prediction_image, cfg, is_categorical=True)
+        visualize_data_gif(labelled_imgs, predicted_imgs, overlay_predicted_imgs, cfg)
+
     else:
         logger.info('No option selected')
         sys.exit()
--- utils/visualizer.py
+++ utils/visualizer.py
@@ -8,7 +8,25 @@ import cv2
 import sys
 from IPython.display import Image
 import imageio
+from tqdm import tqdm
 
+def to_categorical(mask, num_classes):
+    """ 
+    1-hot encoding of mask into number of classes mentioned.
+    
+    Parameters
+    ----------
+    mask: np.ndarray (label of the image)
+    num_classes: int (number of tumor classes)
+
+    Returns
+    -------
+    encoded: np.ndarray (one hot encoded numpy array)
+    """
+    # print(f'mask data type: {mask[:, :, 78].max(), mask[:, :, 78].min()}')
+    # create one hot encoding of the label...
+    # print(mask.shape, "- mask shape")
+    return np.eye(num_classes, dtype= np.uint8)[mask]
 
 def channel_last(image, label):
     """
@@ -39,6 +57,38 @@ def channel_last(image, label):
     return (image, label)
 
 
+def show_channel_last(image, label, prediction):
+    """
+    convert to channel last as expected by some functions 
+    
+    Parameters
+    ----------
+    image: torch.Tensor (an MRI scan consists of 4 channels)
+    label: torch.Tensor (1개의 label에 대한 scan) 
+
+    Returns
+    -------
+    data: tuple(np.ndarray, np.ndarray) a processed sample
+    """
+    # input으로 단일 채널의 label만 들어와야 함. 즉, (1, 1, W, H, D) 형태여야 함.
+
+    #remove batch dim
+    if len(image.shape) == 5:
+        image      = image.squeeze(0)
+        label      = label.squeeze(0)
+        prediction = prediction.squeeze(0)
+
+    #conver to last channel from (c, d, h, w) --> (h, w, d, c)
+    #label (d, h, w) --> (h, w, d)
+    image      = image.permute(2, 3, 1, 0)
+    label      = label.permute(2, 3, 1, 0)
+    prediction = prediction.permute(2, 3, 1, 0)
+    image      = image.numpy()
+    label      = label.numpy()
+    prediction = prediction.numpy()
+    return image, label, prediction
+
+
 def get_labelled_image(image, label, is_categorical = False):
     """
     get an MRI scan labelled with annotated label by a radiologist
@@ -56,11 +106,11 @@ def get_labelled_image(image, label, is_categorical = False):
     labeled_image: np.ndarray (a labelled image for visualization)
     """
     #convert to channel last 
-    image, label = channel_last(image, label)
+    image, label = show_channel_last(image, label)
 
     # convert to one hot encoding
-    if not is_categorical:
-        label = to_categorical(label.astype(np.uint8), num_classes= 4)
+    # if not is_categorical:
+    #     label = to_categorical(label.astype(np.uint8), num_classes= 4)
     
     # normalize the image
     image = cv2.normalize(image[:, :, :, 0], None, alpha=0, beta=255,
@@ -77,28 +127,124 @@ def get_labelled_image(image, label, is_categorical = False):
     labeled_image += label[:, :, :, 1:] * 255
     return labeled_image
 
+def get_show_image(image, labels, predictions, cfg, is_categorical = False):
+    modality = cfg.show.modality
+    index2modality = {0: "T1n", 1: "T1c", 2: "T2w", 3: "T2f"}
+    modality2index = {value:key for key, value in index2modality.items()}
+    
+    image, labels, predictions = show_channel_last(image, labels, predictions)
 
-# TODO: prediction image와 label image를 함께 보여주어 차이를 보는 Voxel을 생성하는 함수.
-def get_prediction_label_image(image, label, prediction):
-    # image     : [B, 4, W, H, D] -> 4개의 modality
-    # label     : [B, 3, W, H, D] -> Background를 제외한 3개의 image
-    # prediction: [B, 3, W, H, D] -> label과 동일한 형태
-    raise NotImplementedError
+    if not is_categorical:
+        labels = to_categorical(labels.astype(np.uint8), num_classes= 4)
 
+    image = cv2.normalize(image[:, :, :, modality2index[modality]], None, alpha=0, beta=255,
+                          norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F).astype(np.uint8)
 
-def visualize_data_gif(data_):
+    labeled_image_list = [image.copy()]
+    predicted_images_list = [image.copy()]
+    overlay_images_list = [image.copy()]
+    class_colors = {
+        0: [255, 0, 0],    # Class 1 (Red)
+        1: [0, 255, 0],    # Class 2 (Green)
+        2: [0, 0, 255]     # Class 3 (Blue)
+    }
+    alpha = 0.5
+    for idx in tqdm(range(labels.shape[-1]), desc="Processing images"):
+        mask = (labels[:, :, :, idx] == 1)  # 특정 클래스에 해당하는 마스크
+        prediction = (predictions[:, :, :, idx] == 1)  
+        labeled_image =  np.stack([image] * 3, axis=-1)  
+        predicted_image = np.stack([image] * 3, axis=-1) 
+        overlay_predicted_image = np.stack([image] * 3, axis=-1)  
+        for channel in range(3):
+            labeled_image[..., channel] = labeled_image[..., channel] * (1 - alpha * mask) \
+                + alpha * mask * class_colors[idx][channel]
+            # non-overlay
+            predicted_image[..., channel] = predicted_image[..., channel] * (1 - alpha * prediction) \
+                + alpha * prediction * class_colors[idx][channel]     
+            # overlay
+            overlay_predicted_image[..., channel] = overlay_predicted_image[..., channel] * (1 - alpha * prediction) \
+                + (alpha / 2) * prediction * class_colors[(idx + 1) % 3][channel] + (alpha / 2) * mask * class_colors[idx][channel]
+            
+        labeled_image_list.append(labeled_image)
+        predicted_images_list.append(predicted_image)
+        overlay_images_list.append(overlay_predicted_image)
+
+    return labeled_image_list, predicted_images_list, overlay_images_list
+
+def process_images(image_list, processed_images):
+        """
+        이미지를 처리하고 결과를 저장합니다.
+        
+        Args:
+            image_list (list): 처리할 이미지들의 리스트.
+            processed_images (list): 처리된 이미지를 저장할 리스트.
+        """
+        for idx, data_ in enumerate(tqdm(image_list, desc="Concatenating images")):
+            for i in range(data_.shape[0]):
+                x = data_[min(i, data_.shape[0] - 1), :, :]
+                y = data_[:, min(i, data_.shape[1] - 1), :]
+                z = data_[:, :, min(i, data_.shape[2] - 1)]
+                img = np.concatenate((x, y, z), axis=1)
+                processed_images[idx].append(img)
+
+# 각 이미지 리스트에 대해 처리 함수 호출
+
+def save_gif(images, folder_path, name, cfg, mapping_table, label_type):
+    """
+    GIF 파일을 저장하는 함수.
+
+    Args:
+        images (list): 저장할 이미지 리스트.
+        folder_path (str): 저장할 폴더 경로.
+        name (str): 파일 이름에 포함될 이름.
+        patient_id (str): 환자 ID.
+        mapping_table (list): 매핑 테이블.
+        label_type (str): 라벨 유형 ('label', 'prediction', 'overlay').
+    """
+    patient_id = cfg.show.patient_id
+    modality = cfg.show.modality
+    for idx, img in enumerate(tqdm(images, desc="Saving GIFs")):
+        file_path = f"{folder_path}/{name}_{label_type}_{patient_id}_{modality}_{mapping_table[idx]}.gif"
+        imageio.mimsave(file_path, img, duration=0.5, format='GIF', loop=0)
+
+def visualize_data_gif(labeld_img_list, predicted_img_list, overlay_predicted_img_list, cfg, name="gif"):
+    mapping_table = {0: "BG", 1: "ET", 2: "TC", 3: "WT"}
+    labeled_images = [[] for _ in range(len(labeld_img_list))]
+    predicted_images = [[] for _ in range(len(predicted_img_list))]
+    overlay_predicted_images = [[] for _ in range(len(overlay_predicted_img_list))]
+
+    process_images(labeld_img_list, labeled_images)
+    process_images(predicted_img_list, predicted_images)
+    process_images(overlay_predicted_img_list, overlay_predicted_images)
+    os.makedirs(f"./runs/label", exist_ok=True)
+    os.makedirs(f"./runs/overlay", exist_ok=True)
+    os.makedirs(f"./runs/prediction", exist_ok=True)
+
+    save_gif(labeled_images, "./runs/label", name, cfg, mapping_table, "label")
+    save_gif(predicted_images, "./runs/prediction", name, cfg, mapping_table, "prediction")
+    save_gif(overlay_predicted_images, "./runs/overlay", name, cfg, mapping_table, "overlay")
+
+        
+    return 
+        
+
+def visualize_data_list_gif(data_, name="gif"):
     images = []
-    for i in range(data_.shape[0]):
-        x = data_[min(i, data_.shape[0] - 1), :, :]
-        y = data_[:, min(i, data_.shape[1] - 1), :]
-        z = data_[:, :, min(i, data_.shape[2] - 1)]
+    for label in range(3):
+        temp_img = []
+        for i in range(data_.shape[0]):
+            x = data_[min(i, data_.shape[0] - 1), :, :]
+            y = data_[:, min(i, data_.shape[1] - 1), :]
+            z = data_[:, :, min(i, data_.shape[2] - 1)]
+            temp_img.extend([x, y, z])
+        
         img = np.concatenate((x, y, z), axis=1)
         images.append(img)
     os.makedirs(f"../runs", exist_ok=True)
     try:
-        imageio.mimsave(f"../runs/gif.gif", images, duration=0.5, format='GIF', loop = 0)
-        return Image(filename=f"../runs/gif.gif", format='png')
+        imageio.mimsave(f"../runs/{name}.gif", images, duration=0.5, format='GIF', loop = 0)
+        return Image(filename=f"../runs/{name}.gif", format='png')
     except FileNotFoundError:
         root = Path(__file__).resolve().parents[1]
-        imageio.mimsave(f"{root}/runs/gif.gif", images, duration=0.5, format='GIF', loop = 0)
-        return Image(filename=f"{root}/runs/gif.gif", format='png')
+        imageio.mimsave(f"{root}/runs/{name}.gif", images, duration=0.5, format='GIF', loop = 0)
+        return Image(filename=f"{root}/runs/{name}.gif", format='png')
